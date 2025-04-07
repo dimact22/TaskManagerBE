12dd95faa9f9c6683cbafaffa7e485a7c21f4696
Dmytro <dmytro.shelikhov@creativedock.com>
2025-04-07 16:19:37 +0200

    Змінна лвл логування

--- logger.py
+++ logger.py
@@ -1,8 +1,13 @@
 import logging
+import os
+
+log_level_str = os.getenv("LOG_LEVEL", "INFO").upper()
+
+log_level = getattr(logging, log_level_str, logging.INFO)
 
 # Базове налаштування логування
 logging.basicConfig(
-    level=logging.INFO,
+    level=log_level,
     format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
